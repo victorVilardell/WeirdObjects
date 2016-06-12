f8e086a5bd3bbbb09e7e73502ccee3b133decbfa
victorVilardell <vvilard@gmail.com>
2016-06-12 23:52:10 +0200

    js oop
    Falta corregir un bucle

--- js/jsonFlickr.js
+++ js/jsonFlickr.js
@@ -32,49 +32,52 @@ $.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhot
 });
 
 })(document)*/
-
+/*
 (function() {
 
 	function WeirdObjects() {
 		this.numImage = 4; //numero de imagenes por pagina
 		this.numPage = 0; //numero de pagina actual
-		this.dataFlickr = {};
 		this.content = $(".pc");
-	}
-
-	WeirdObjects.prototype.getJsonFlickr = function() {
-		var data = $.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&name=value&format=json&asinc=1&api_key=aed656c0441370035c9c6e23ddb2c471&gallery_id=9634-72157621980433950&extras=description,owner_name&per_page="+this.numImg+"&page="+this.numPage+"&jsoncallback=?", function () {});
-		this.dataFlickr = data;
-		return this.dataFlickr;
-	}
-
-	WeirdObjects.prototype.constructor = function(dataFlickr) {
-		var info = this.dataFlickr;
 		
+		this.getJsonFlickr = function() {
+			this.xx = function(e) { 
+				$(".pc").find(".fa-cog").remove();
+			    WeirdObjects.xx();
+				var JSONData = e.photos.photo;
+				for (var i = (WeirdObjects.numImage * WeirdObjects.numPage); i <= ((WeirdObjects.numImage * WeirdObjects.numPage) + WeirdObjects.numImage) ; i++ ) {
+					var imge = $("<img>").attr("src", "https://farm"+ JSONData[i].farm +".staticflickr.com/"+ JSONData[i].server +"/"+ JSONData[i].id +"_"+ JSONData[i].secret +".jpg");
+					var link = $("<a/>").append(imge).attr("title", JSONData[i].title)
+							.attr("href", "https://www.flickr.com/photos/"+ JSONData[i].owner +"/"+ JSONData[i].id );
+					$(selecArg[i]).append(link);
+				}
+			};
+			$.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&name=value&format=json&asinc=1&api_key=aed656c0441370035c9c6e23ddb2c471&gallery_id=9634-72157621980433950&extras=description,owner_name&per_page="+this.numImg+"&page="+this.numPage+"&jsoncallback=?", function (data) {
+			})
+			.done(function(e){
+				this.xx(e)
+			})
+			.fail(function() {
+				var error = "Sorry the service is not available.";
+				var errorMsg = $("<div class='alert alert-warning' role='alert'/>").append(error);
+				$(this.content).append(errorMsg);
+			})
+			.always(function() {
+				/* eliminar siempre el loader
+			});
+			}
+	}
 
-		if (info != undefined) {
-				for(var i=0; i <= this.numImage-1; i++) {
-			    	var loader = $("<i class='fa fa-cog fa-spin fa-3x fa-fw'></i>");
-			    	var article = $("<article class='col-lg-3 col-md-6 col-sm-6 col-xs-12'/>").append(loader);
-					$(this.content).append(article)
-	    		}
-	    		this.printImage(this.numImage);
-			
-		} else {
-			var error = "Sorry the service is not available.";
-			var errorMsg = $("<div class='alert alert-warning' role='alert'/>").append(error);
-			$(this.content).append(errorMsg)
+	
+
+	WeirdObjects.prototype.constructor = function() {
+
+		for(var i=0; i <= this.numImage-1; i++) {
+	    	var loader = $("<i class='fa fa-cog fa-spin fa-3x fa-fw'></i>");
+	    	var article = $("<article class='col-lg-3 col-md-6 col-sm-6 col-xs-12'/>").append(loader);
+			$(this.content).append(article);
 		}
 
-	}
-
-	WeirdObjects.prototype.printImage = function(getImage) {
-		$(this.content).find(".fa-cog").remove();
-		console.log(this.dataFlickr.responseJSON);
-		var imge = $("<img>").attr("src", "https://farm"+ thisDate.farm +".staticflickr.com/"+ thisDate.server +"/"+ thisDate.id +"_"+ thisDate.secret +".jpg");
-		var link = $("<a/>").append(imge).attr("title", thisDate.title)
-				.attr("href", "https://www.flickr.com/photos/"+ thisDate.owner +"/"+ thisDate.id );
-		$(selecArg[i]).append(link);
 	}
 
 	WeirdObjects.prototype.init = function() {
@@ -85,4 +88,75 @@ $.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhot
 	var go = new WeirdObjects();
 	go.init();
 
-})(document)
+})(document)*/
+
+var WeirdObjects =  {
+
+	paginator: {
+		numImage: 4, //numero de imagenes por pagina
+		numPage : 0, //numero de pagina actual
+	},
+
+	content: $(".pc"),
+
+	JSONData: 0,
+
+	getJsonFlickr: function() {
+		$.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&name=value&format=json&asinc=1&api_key=aed656c0441370035c9c6e23ddb2c471&gallery_id=9634-72157621980433950&extras=description,owner_name&per_page="+this.numImg+"&page="+this.numPage+"&jsoncallback=?", function (data) {
+		})
+		.done(function(e){
+			WeirdObjects.loadImages(e)
+		})
+		.fail(function() {
+			var error = "Sorry the service is not available.";
+			var errorMsg = $("<div class='alert alert-warning' role='alert'/>").append(error);
+			$(this.content).append(errorMsg);
+		})
+		.always(function() {
+			/* eliminar siempre el loader*/
+		});
+	},
+
+	loadImages: function(e) {
+		var contentArticle = $(".pc article");
+		$(this.content).find(".fa-cog").remove();
+		if (this.JSONData == 0) {
+			this.JSONData = e.photos.photo;
+		};	
+		for (var i = (this.paginator.numImage * this.paginator.numPage); i <= ((this.paginator.numImage * this.paginator.numPage) + this.paginator.numImage) ; i++ ) {
+			var xx = 0;
+			var imge = $("<img>").attr("src", "https://farm"+ this.JSONData[i].farm +".staticflickr.com/"+ this.JSONData[i].server +"/"+ this.JSONData[i].id +"_"+ this.JSONData[i].secret +".jpg");
+			var link = $("<a/>").append(imge).attr("title", this.JSONData[i].title)
+					.attr("href", "https://www.flickr.com/photos/"+ this.JSONData[i].owner +"/"+ this.JSONData[i].id );
+			$(contentArticle[xx]).append(link);
+			xx++
+		}
+	},
+
+	constructor: function() {
+
+		for(var i=0; i <= this.paginator.numImage-1; i++) {
+	    	var loader = $("<i class='fa fa-cog fa-spin fa-3x fa-fw'></i>");
+	    	var article = $("<article class='col-lg-3 col-md-6 col-sm-6 col-xs-12'/>").append(loader);
+			$(this.content).append(article);
+		}
+
+	},
+
+	events: function() {
+		$(".next").bind("click", function(e) {
+			e.preventDefault();
+			WeirdObjects.paginator.numPage = WeirdObjects.paginator.numPage + 1;
+			WeirdObjects.loadImages();
+		})
+
+	},
+
+	init: function() {
+		this.getJsonFlickr();
+		this.constructor();
+		this.events();
+	}
+}
+
+WeirdObjects.init();
