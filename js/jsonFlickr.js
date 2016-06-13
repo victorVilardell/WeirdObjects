a3842167b047e81fcf14e22b39a4e6a742c27fbe
dir <VictorVilardell@web-vv-22418.dorna.net>
2016-06-13 09:11:16 +0200

    paginator

--- js/jsonFlickr.js
+++ js/jsonFlickr.js
@@ -1,95 +1,3 @@
-/*
-var numImg = 4;
-var numPage = 0;
-
-(function(){
-
-$.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&name=value&format=json&asinc=1&api_key=aed656c0441370035c9c6e23ddb2c471&gallery_id=9634-72157621980433950&extras=description,owner_name&per_page="+numImg+"&page="+numPage+"&jsoncallback=?", function (data) {
-
-	var elemPrint = $(".pc");
-
-	if (data.stat != 1) {
-		for(i=0; i <= numImg-1; i++) {
-	    	var loader = $("<i class='fa fa-cog fa-spin fa-3x fa-fw'></i>");
-	    	var article = $("<article class='col-lg-3 col-md-6 col-sm-6 col-xs-12'/>").append(loader);
-			$(elemPrint).append(article)
-    	}
-	}
-
-    for(i=0; i <= numImg-1; i++) {
-    	var thisDate = data.photos.photo[i];
-    	var selecArg = $(".pc article");
-
-		$(selecArg[i]).find(".fa-cog").remove();
-		var imge = $("<img>").attr("src", "https://farm"+ thisDate.farm +".staticflickr.com/"+ thisDate.server +"/"+ thisDate.id +"_"+ thisDate.secret +".jpg");
-		var link = $("<a/>").append(imge).attr("title", thisDate.title)
-				.attr("href", "https://www.flickr.com/photos/"+ thisDate.owner +"/"+ thisDate.id );
-		$(selecArg[i]).append(link);
-
-    }
-
-
-});
-
-})(document)*/
-/*
-(function() {
-
-	function WeirdObjects() {
-		this.numImage = 4; //numero de imagenes por pagina
-		this.numPage = 0; //numero de pagina actual
-		this.content = $(".pc");
-		
-		this.getJsonFlickr = function() {
-			this.xx = function(e) { 
-				$(".pc").find(".fa-cog").remove();
-			    WeirdObjects.xx();
-				var JSONData = e.photos.photo;
-				for (var i = (WeirdObjects.numImage * WeirdObjects.numPage); i <= ((WeirdObjects.numImage * WeirdObjects.numPage) + WeirdObjects.numImage) ; i++ ) {
-					var imge = $("<img>").attr("src", "https://farm"+ JSONData[i].farm +".staticflickr.com/"+ JSONData[i].server +"/"+ JSONData[i].id +"_"+ JSONData[i].secret +".jpg");
-					var link = $("<a/>").append(imge).attr("title", JSONData[i].title)
-							.attr("href", "https://www.flickr.com/photos/"+ JSONData[i].owner +"/"+ JSONData[i].id );
-					$(selecArg[i]).append(link);
-				}
-			};
-			$.getJSON("https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&name=value&format=json&asinc=1&api_key=aed656c0441370035c9c6e23ddb2c471&gallery_id=9634-72157621980433950&extras=description,owner_name&per_page="+this.numImg+"&page="+this.numPage+"&jsoncallback=?", function (data) {
-			})
-			.done(function(e){
-				this.xx(e)
-			})
-			.fail(function() {
-				var error = "Sorry the service is not available.";
-				var errorMsg = $("<div class='alert alert-warning' role='alert'/>").append(error);
-				$(this.content).append(errorMsg);
-			})
-			.always(function() {
-				/* eliminar siempre el loader
-			});
-			}
-	}
-
-	
-
-	WeirdObjects.prototype.constructor = function() {
-
-		for(var i=0; i <= this.numImage-1; i++) {
-	    	var loader = $("<i class='fa fa-cog fa-spin fa-3x fa-fw'></i>");
-	    	var article = $("<article class='col-lg-3 col-md-6 col-sm-6 col-xs-12'/>").append(loader);
-			$(this.content).append(article);
-		}
-
-	}
-
-	WeirdObjects.prototype.init = function() {
-		this.getJsonFlickr();
-		this.constructor();
-	}
-
-	var go = new WeirdObjects();
-	go.init();
-
-})(document)*/
-
 var WeirdObjects =  {
 
 	paginator: {
@@ -98,7 +6,6 @@ var WeirdObjects =  {
 	},
 
 	content: $(".pc"),
-
 	JSONData: 0,
 
 	getJsonFlickr: function() {
@@ -119,18 +26,26 @@ var WeirdObjects =  {
 
 	loadImages: function(e) {
 		var contentArticle = $(".pc article");
+		var xx = 0;
 		$(this.content).find(".fa-cog").remove();
 		if (this.JSONData == 0) {
 			this.JSONData = e.photos.photo;
 		};	
+		
 		for (var i = (this.paginator.numImage * this.paginator.numPage); i <= ((this.paginator.numImage * this.paginator.numPage) + this.paginator.numImage) ; i++ ) {
-			var xx = 0;
 			var imge = $("<img>").attr("src", "https://farm"+ this.JSONData[i].farm +".staticflickr.com/"+ this.JSONData[i].server +"/"+ this.JSONData[i].id +"_"+ this.JSONData[i].secret +".jpg");
 			var link = $("<a/>").append(imge).attr("title", this.JSONData[i].title)
 					.attr("href", "https://www.flickr.com/photos/"+ this.JSONData[i].owner +"/"+ this.JSONData[i].id );
 			$(contentArticle[xx]).append(link);
 			xx++
 		}
+	},
+
+	cleanImage: function(){
+		var contentAncor = $(".pc article a");
+		$(contentAncor).each(function(i){
+			$(this).remove();
+		})
 	},
 
 	constructor: function() {
@@ -141,12 +56,30 @@ var WeirdObjects =  {
 			$(this.content).append(article);
 		}
 
+		var paginator = "<p><a href='#' class='btn btn-primary prev'>prev</a><a href='#' class='btn btn-primary next'>next</a></p>"
+		$(paginator).insertAfter( this.content );
 	},
 
 	events: function() {
 		$(".next").bind("click", function(e) {
 			e.preventDefault();
-			WeirdObjects.paginator.numPage = WeirdObjects.paginator.numPage + 1;
+
+			if (WeirdObjects.paginator.numPage < Math.floor(WeirdObjects.JSONData.length / WeirdObjects.paginator.numImage) ) {
+				WeirdObjects.paginator.numPage++;
+			}
+
+			WeirdObjects.cleanImage();
+			WeirdObjects.loadImages();
+		});
+
+		$(".prev").bind("click", function(e) {
+			e.preventDefault();
+
+			if (WeirdObjects.paginator.numPage > 0 ){
+				WeirdObjects.paginator.numPage--;
+			};
+
+			WeirdObjects.cleanImage();
 			WeirdObjects.loadImages();
 		})
 
